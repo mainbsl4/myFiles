f82eb3ab9ac97a1349829a8f6cb4baa05dda73e2
Md. Main <76824119+mainbsl4@users.noreply.github.com>
2022-08-22 23:47:40 +0600

    Update fanction.js

--- fanction.js
+++ fanction.js
@@ -1,1 +1,22 @@
+{
+    function profiles (fName, lName, cls, age) {
+        this.fName = fName;
+        this.lName = lName;
+        this.cls = cls;
+        this.age = new Date(age);
 
+        this.fullName = ()=>{
+            return(
+                `${this.fName} ${this.lName}`
+            )
+        }
+        this.getDay = () => {
+            return(
+                `${this.age.getDay() + 1}`
+            )
+        }
+    }
+
+    const student1 = new profiles('md', 'main', 12, '11-12-2004')
+    console.log(student1.getDay());
+}
