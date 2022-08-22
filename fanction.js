bbb45059e4f48a05a96b857838eabb8f3caeb1b6
Md. Main <76824119+mainbsl4@users.noreply.github.com>
2022-08-23 00:28:29 +0600

    Update fanction.js

--- fanction.js
+++ fanction.js
@@ -9,14 +9,14 @@
             return(
                 `${this.fName} ${this.lName}`
             )
-        }
+        };
         this.getDay = () => {
             return(
                 `${this.age.getDay() + 1}`
             )
-        }
-    }
+        };
+    };
 
-    const student1 = new profiles('md', 'main', 12, '11-12-2004')
+    const student1 = new profiles('md', 'main', 12, '11-12-2004');
     console.log(student1.getDay());
 }
