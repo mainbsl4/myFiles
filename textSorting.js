5ad9b3d887035255bfa27cfca532a28b56e69720
Md. Main <76824119+mainbsl4@users.noreply.github.com>
2022-08-22 23:46:42 +0600

    Update textSorting.js

--- textSorting.js
+++ textSorting.js
@@ -1,1 +1,15 @@
+{
+    const text = ["Banana", "apple", "Mango"]
 
+        text.sort(function (a, b) {
+            let x =  a.toLowerCase();
+            let y =  b.toLowerCase();
+    
+            if(x < y){return -1;};
+    
+            if(x > y){return 1;};
+    
+            return 0;
+        });
+        console.log(text);
+};
