92475ffcdfdaea8bf37d72f2003d0d57fb9acba1
Md. Main <76824119+mainbsl4@users.noreply.github.com>
2022-08-22 23:47:10 +0600

    Update numberSorting.js

--- numberSorting.js
+++ numberSorting.js
@@ -1,1 +1,15 @@
+{
+    const numbers = [2, 5, 1, 4, 3];
 
+    function addBtn(){
+        for(let i = numbers.length - 1; i > 0; i--){
+            j = Math.floor(Math.random() * i);
+            k = numbers[i];
+
+            numbers[i] = numbers[j];
+            numbers[j] = k;
+        }
+        console.log(numbers);
+    }
+    
+}
